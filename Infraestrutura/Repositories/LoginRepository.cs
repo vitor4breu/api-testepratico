dab5bb6a703dfe3f094215b3bcf7197b7ebfa35c
vitor4breu <vitor148197@hotmail.com>
2022-08-24 22:21:34 -0300

    Estruturacao da camada de infraestrutura

---
+++ Infraestrutura/Repositories/LoginRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Interfaces.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class LoginRepository : ILoginRepository
+    {
+        public bool SelectLogin(string username, string password)
+        {
+            return true;
+        }
+    }
+}
---
+++ Infraestrutura/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Interfaces.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public IEnumerable<string> SelectUsers()
+        {
+            return new List<string>();
+        }
+    }
+}
