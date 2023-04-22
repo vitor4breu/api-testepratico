21e6c5907e0d1ee4708ab8ce288acb84a3516d3d
Vitor Abreu <vitor148197@hotmail.com>
2023-04-22 04:35:09 -0300

    Correção do objeto de mensagemRetorno

--- Dominio/Retorno/MensagemRetorno.cs
+++ Dominio/Retorno/MensagemRetorno.cs
@@ -2,8 +2,8 @@
 {
     public class MensagemRetorno
     {
-        protected List<string> Erros { get; } = new List<string>();
-        protected List<string> Avisos { get; } = new List<string>();
+        public List<string> Erros { get; } = new List<string>();
+        public List<string> Avisos { get; } = new List<string>();
 
         public bool ContemErro => Erros.Count > 0;
         public bool ContemAviso => Avisos.Count > 0;
